37ab3858f7a268844b299299fb556313a918831e
shehab299 <shehab299@outlook.com>
2025-02-06 03:26:21 +0200

    fixed shop doesn't support images

--- server/src/services/shop.service.js
+++ server/src/services/shop.service.js
@@ -3,12 +3,12 @@ const AppError = require("../errors/AppError");
 
 class ShopService {
 
-    static async createShop(userId, { name, description }) 
+    static async createShop(userId, body) 
     {
-        return await Shop.create({ name, description, userId });
+        return await Shop.create(body);
     }
 
-    static async updateShop(userId, shopId, { name, description }) {
+    static async updateShop(userId, shopId, body) {
         const shop = await Shop.findByPk(shopId);
         if (!shop) {
             throw new AppError("Shop not found", 404);
@@ -18,8 +18,10 @@ class ShopService {
             throw new AppError("Unauthorized: You are not the owner of this shop", 401);
         }
 
-        shop.name = name || shop.name;
-        shop.description = description || shop.description;
+        shop.name = body.name || shop.name;
+        shop.description = body.description || shop.description;
+        shop.logo = body.logo || shop.description;
+        shop.banner = body.banner || shop.description;
 
         await shop.save();
         return shop;
--- server/src/validations/Models/shop.validation.js
+++ server/src/validations/Models/shop.validation.js
@@ -4,8 +4,8 @@ const Joi = require('joi');
 const shopSchema = Joi.object({
     name: Joi.string().min(2).max(50).required(),
     description: Joi.string().min(2).max(255).required(),
-    logo: Joi.string().uri(),
-    banner: Joi.string().uri()
+    logo: Joi.string().uri().optional(),
+    banner: Joi.string().uri().optional()
 });
 
 
